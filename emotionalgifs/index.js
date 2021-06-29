9bfb92a02668fa08b03dfc63cf7be694d7caf04d
Christina Buencamino <c.buencamino123@gmail.com>
2021-06-29 02:15:10 -0400

    fixed dom emotion function, this time without leaking my security information

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -43,13 +43,13 @@ module.exports = async function (context, req) {
 
 /* note: we use async when we are calling another API */
 async function analyzeImage(img){
-    /*//use for cloud testing/deploying
+    //use for cloud testing/deploying
     const subscriptionKey = process.env.SUBSCRIPTIONKEY; //process.env = how to access secrets, cannot be used locally
-    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect'; */
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect'; 
 
-    //use for local testing ***MUST delete key and base if deploying - security risk!
-    const subscriptionKey = "2091265e244e4a8da1e5e516ea6b7c18"
-    const uriBase = "https://emotionalapiserverless.cognitiveservices.azure.com/face/v1.0/detect" 
+    /* //use for local testing ***MUST delete key and base if deploying - security risk!
+    const subscriptionKey = "insert subscription key"
+    const uriBase = "insert url" */
 
 
     let params = new URLSearchParams({
