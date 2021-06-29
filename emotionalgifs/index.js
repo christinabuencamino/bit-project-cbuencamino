0409215dc2ae485c1ad551a172d412c0280a3502
Christina Buencamino <c.buencamino123@gmail.com>
2021-06-29 01:17:13 -0400

    add face api fetch call

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,4 +1,5 @@
 var multipart = require('parse-multipart');
+var fetch = require('node-fetch')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -12,11 +13,49 @@ module.exports = async function (context, req) {
 
     var imageData = parts[0].data
 
-    /* convert result into base 64 */
-    var convertedResult = Buffer.from(imageData).toString('base64');
-
+    /*
+    //convert result into base 64 
+    var convertedResult = Buffer.from(imageData).toString('base64'); */
+    /*
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        // status: 200, Defaults to 200 
+        body: convertedResult 
+    }; */
+
+    //module.exports function
+    //analyze the image
+        
+    var result = await analyzeImage(imageData);
+    context.res = {
+	    body: {
+		    result
+	    }
     };
+
+    console.log(result) //console logging the results so we can see any errors in the console for debugging
+    context.done(); 
+}
+
+/* note: we use async when we are calling another API */
+async function analyzeImage(img){
+    const subscriptionKey = process.env.SUBSCRIPTIONKEY; //process.env = how to access secrets, cannot be used locally
+    const uriBase = process.env.ENDPOINT + '/face/v1.0/detect'; 
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'    
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',  
+        body: img,  
+      
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }  
+    })
+
+    let emotionData = await resp.json() //receiving the data using fetch
+    return emotionData 
 }
