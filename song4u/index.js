ebd078077da86dafe9f5b97ee626875c39e85a29
Christina Buencamino <c.buencamino123@gmail.com>
2021-06-30 16:09:54 -0400

    trying to fix age/generation results

--- song4u/index.js
+++ song4u/index.js
@@ -1,6 +1,5 @@
 const querystring = require('querystring');
 var fetch = require('node-fetch');
-const { Agent } = require('http');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
@@ -32,30 +31,38 @@ module.exports = async function (context, req) {
     let age = result[0].faceAttributes.age
 
     if (age >= 5 && age <= 25) {
-        id = "GenZ"
+        generation = "GenZ"
     }
     else if (age >= 26 && age <= 41) {
-        id = "GenY"
+        generation = "GenY"
     }
     else if (age >= 42 && age <= 57) {
-        id = "GenX"
+        generation = "GenX"
     }
     else if (age >= 58 && age <= 76) {
-        id = "BabyBoomers"
+        generation = "BabyBoomers"
     } 
     else {
-        id = "Unknown"
+        generation = "Unknown"
     }
 
     context.res = {
-        body: id
-     };     
+        body: generation
+     };   
+     
+    console.log(generation) //console logging the results so we can see any errors in the console for debugging
+    context.done(); 
 }
 
 async function analyzeImage(img){
     const subscriptionKey = process.env.SUBSCRIPTIONKEY; //process.env protects personal data from being exposed in code
     const uriBase = process.env.ENDPOINT + '/face/v1.0/detect';
 	// env variables (similar to .gitignore/.env file) to not expose personal info
+
+     
+    //use for local testing ***MUST delete key and base if deploying - security risk!
+    //const subscriptionKey = ""
+    //const uriBase = ""
 
     let params = new URLSearchParams({
 	'returnFaceId': 'true',
@@ -74,8 +81,8 @@ async function analyzeImage(img){
     })
 
     // receive the response
-    let data = await resp.json();
+    let generationData = await resp.json();
 
-    return data;
+    return generationData;
 }
 
